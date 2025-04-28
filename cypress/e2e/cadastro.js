957364f15b8f1f9e6b4341fd9223dc47b38796f4
adriely.novais <adriely.novais@cepedi.org.br>
2025-04-27 22:02:50 -0300

    Automação com cypress

---
+++ cypress/e2e/cadastro.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress"/>
+
+describe('US-012-Funcionalidade: cadastro de membros', () => {
+  beforeEach(() =>{
+    cy.visit('/')
+  })
+  
+  it('Deve fazer o cadastro de campos obrigatórios', () => {
+    var email = `driu${Date.now()}@teste.com`
+    cy.preencherCadastro('Driu', 'Souza', email, '73988774455', 'Teste@1232')
+    cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!' )
+  })
+  it('Deve válidar mensagem de erro com cadastro nome inválido', () => {
+    var email = `driu${Date.now()}@teste.com`
+    cy.preencherCadastro('Driu20', 'Souza', email, '73988774455', 'Teste@1232')
+    cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços' )
+  })
+
+
+})
